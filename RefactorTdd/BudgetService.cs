a1e1cba22af110f523df982f7412999d5dccf46c
Joey Chen <jay.hatelove@gmail.com>
2018-11-10 19:55:34 +0800

    rename and mirgrate nuget

--- RefactorTdd/BudgetService.cs
+++ RefactorTdd/BudgetService.cs
@@ -3,83 +3,83 @@ using System.Linq;
 
 namespace RefactorTdd
 {
-	public class BudgetService
-	{
-		private readonly IBudgetRepo _repo;
+    public class BudgetService
+    {
+        private readonly IBudgetRepo _repo;
 
-		private static void Main()
-		{
-		}
+        private static void Main()
+        {
+        }
 
-		public BudgetService(IBudgetRepo repo)
-		{
-			_repo = repo;
-		}
+        public BudgetService(IBudgetRepo repo)
+        {
+            _repo = repo;
+        }
 
-		public double TotalAmount(DateTime start, DateTime end)
-		{
-			if (!IsValidDateRange(start, end))
-			{
-				return 0;
-			}
+        public double TotalAmount(DateTime start, DateTime end)
+        {
+            if (!IsValidDateRange(start, end))
+            {
+                return 0;
+            }
 
-			var budgets = _repo.GetAll();
+            var budgets = _repo.GetAll();
 
-			if (IsSameMonth(start, end))
-			{
-				var budget = budgets.SingleOrDefault(x => x.YearMonth.Equals(start.ToString("yyyyMM")));
-				if (budget == null)
-				{
-					return 0;
-				}
+            if (IsSameMonth(start, end))
+            {
+                var budget = budgets.SingleOrDefault(x => x.YearMonth.Equals(start.ToString("yyyyMM")));
+                if (budget == null)
+                {
+                    return 0;
+                }
 
-				var budgetPerDay = budget.Amount / DateTime.DaysInMonth(start.Year, start.Month);
-				return budgetPerDay * DaysInterval(start, end);
-			}
-			else
-			{
-				DateTime tempDate = new DateTime(start.Year, start.Month, 1);
-				double aggrAmount = 0;
-				do
-				{
-					var budgetByMonth =
-						budgets.SingleOrDefault(x => x.YearMonth.Equals(tempDate.ToString("yyyyMM")));
-					if (budgetByMonth != null)
-					{
-						if (tempDate.ToString("yyyyMM") == start.ToString("yyyyMM"))
-							aggrAmount += AmountPerDayInMonth(budgetByMonth, start) *
-										  (DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1);
-						else if (tempDate.ToString("yyyyMM") == end.ToString("yyyyMM"))
-							aggrAmount += AmountPerDayInMonth(budgetByMonth, end) * end.Day;
-						else
-							aggrAmount += budgetByMonth.Amount;
-					}
+                var dailyAmount = budget.Amount / DateTime.DaysInMonth(start.Year, start.Month);
+                return dailyAmount * DaysInterval(start, end);
+            }
+            else
+            {
+                DateTime currentMonth = new DateTime(start.Year, start.Month, 1);
+                double totalAmount = 0;
+                do
+                {
+                    var budgetByMonth =
+                        budgets.SingleOrDefault(x => x.YearMonth.Equals(currentMonth.ToString("yyyyMM")));
+                    if (budgetByMonth != null)
+                    {
+                        if (currentMonth.ToString("yyyyMM") == start.ToString("yyyyMM"))
+                            totalAmount += AmountPerDayInMonth(budgetByMonth, start) *
+                                          (DateTime.DaysInMonth(start.Year, start.Month) - start.Day + 1);
+                        else if (currentMonth.ToString("yyyyMM") == end.ToString("yyyyMM"))
+                            totalAmount += AmountPerDayInMonth(budgetByMonth, end) * end.Day;
+                        else
+                            totalAmount += budgetByMonth.Amount;
+                    }
 
-					tempDate = tempDate.AddMonths(1);
-				} while (tempDate <= end);
+                    currentMonth = currentMonth.AddMonths(1);
+                } while (currentMonth <= end);
 
-				return aggrAmount;
-			}
-		}
+                return totalAmount;
+            }
+        }
 
-		private static bool IsSameMonth(DateTime start, DateTime end)
-		{
-			return start.ToString("yyyyMM") == end.ToString("yyyyMM");
-		}
+        private static bool IsSameMonth(DateTime start, DateTime end)
+        {
+            return start.ToString("yyyyMM") == end.ToString("yyyyMM");
+        }
 
-		private static int AmountPerDayInMonth(Budget budgetByMonth, DateTime tempDate)
-		{
-			return budgetByMonth.Amount / DateTime.DaysInMonth(tempDate.Year, tempDate.Month);
-		}
+        private static int AmountPerDayInMonth(Budget budgetByMonth, DateTime tempDate)
+        {
+            return budgetByMonth.Amount / DateTime.DaysInMonth(tempDate.Year, tempDate.Month);
+        }
 
-		private static bool IsValidDateRange(DateTime start, DateTime end)
-		{
-			return start <= end;
-		}
+        private static bool IsValidDateRange(DateTime start, DateTime end)
+        {
+            return start <= end;
+        }
 
-		private static int DaysInterval(DateTime start, DateTime end)
-		{
-			return end.Subtract(start).Days + 1;
-		}
-	}
+        private static int DaysInterval(DateTime start, DateTime end)
+        {
+            return end.Subtract(start).Days + 1;
+        }
+    }
 }
--- RefactorTddTests/BudgetServiceTests.cs
+++ RefactorTddTests/BudgetServiceTests.cs
@@ -4,101 +4,100 @@ using System.Collections.Generic;
 
 namespace RefactorTdd.Tests
 {
-	[TestClass()]
-	public class BudgetServiceTests
-	{
-		private FakeBudgetRepo _fakeBudgetRepo;
-		private BudgetService _budgetService;
+    [TestClass()]
+    public class BudgetServiceTests
+    {
+        private FakeBudgetRepo _fakeBudgetRepo;
+        private BudgetService _budgetService;
 
-		[TestInitialize]
-		public void Init()
-		{
-			_fakeBudgetRepo = new FakeBudgetRepo();
-			_budgetService = new BudgetService(_fakeBudgetRepo);
-		}
+        [TestInitialize]
+        public void Init()
+        {
+            _fakeBudgetRepo = new FakeBudgetRepo();
+            _budgetService = new BudgetService(_fakeBudgetRepo);
+        }
 
-		[TestMethod()]
-		public void No_budget_return_zero()
-		{
-			var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 4, 1), new DateTime(2018, 4, 3));
-			AmountShouldBe(0, totalAmount);
-		}
+        [TestMethod()]
+        public void No_budget_return_zero()
+        {
+            var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 4, 1), new DateTime(2018, 4, 3));
+            AmountShouldBe(0, totalAmount);
+        }
 
-		[TestMethod()]
-		public void One_Day()
-		{
-			_fakeBudgetRepo.AddBudget("201801", 31);
-			var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 1, 1), new DateTime(2018, 1, 1));
-			AmountShouldBe(1, totalAmount);
-		}
+        [TestMethod()]
+        public void One_Day()
+        {
+            _fakeBudgetRepo.AddBudget("201801", 31);
+            var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 1, 1), new DateTime(2018, 1, 1));
+            AmountShouldBe(1, totalAmount);
+        }
 
-		private void AmountShouldBe(int expected, double actual)
-		{
-			Assert.AreEqual(expected, actual);
-		}
+        private void AmountShouldBe(int expected, double actual)
+        {
+            Assert.AreEqual(expected, actual);
+        }
 
-		[TestMethod()]
-		public void SameMonth_MultiDays()
+        [TestMethod()]
+        public void SameMonth_MultiDays()
+        {
+            _fakeBudgetRepo.AddBudget("201801", 31);
 
-		{
-			_fakeBudgetRepo.AddBudget("201801", 31);
+            var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 1, 1), new DateTime(2018, 1, 3));
+            AmountShouldBe(3, totalAmount);
+        }
 
-			var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 1, 1), new DateTime(2018, 1, 3));
-			AmountShouldBe(3, totalAmount);
-		}
+        [TestMethod()]
+        public void InvalidDayRange()
+        {
+            _fakeBudgetRepo.AddBudget("201801", 31);
 
-		[TestMethod()]
-		public void InvalidDayRange()
-		{
-			_fakeBudgetRepo.AddBudget("201801", 31);
+            var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 1, 3), new DateTime(2018, 1, 1));
+            AmountShouldBe(0, totalAmount);
+        }
 
-			var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 1, 3), new DateTime(2018, 1, 1));
-			AmountShouldBe(0, totalAmount);
-		}
+        [TestMethod()]
+        public void DiffMonth_WithBudgets()
+        {
+            _fakeBudgetRepo.AddBudget("201801", 31);
+            _fakeBudgetRepo.AddBudget("201802", 280);
 
-		[TestMethod()]
-		public void DiffMonth_WithBudgets()
-		{
-			_fakeBudgetRepo.AddBudget("201801", 31);
-			_fakeBudgetRepo.AddBudget("201802", 280);
+            var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 1, 31), new DateTime(2018, 2, 1));
+            AmountShouldBe(11, totalAmount);
+        }
 
-			var totalAmount = _budgetService.TotalAmount(new DateTime(2018, 1, 31), new DateTime(2018, 2, 1));
-			AmountShouldBe(11, totalAmount);
-		}
+        [TestMethod()]
+        public void DiffYear_WithBudgets()
+        {
+            _fakeBudgetRepo.AddBudget("201712", 31000);
+            _fakeBudgetRepo.AddBudget("201801", 31);
 
-		[TestMethod()]
-		public void DiffYear_WithBudgets()
-		{
-			_fakeBudgetRepo.AddBudget("201712", 31000);
-			_fakeBudgetRepo.AddBudget("201801", 31);
+            var totalAmount = _budgetService.TotalAmount(new DateTime(2017, 12, 31), new DateTime(2018, 1, 1));
+            AmountShouldBe(1001, totalAmount);
+        }
 
-			var totalAmount = _budgetService.TotalAmount(new DateTime(2017, 12, 31), new DateTime(2018, 1, 1));
-			AmountShouldBe(1001, totalAmount);
-		}
+        [TestMethod()]
+        public void DiffYear_With_Cross_MultiMonthBudgets()
+        {
+            _fakeBudgetRepo.AddBudget("201712", 31000);
+            _fakeBudgetRepo.AddBudget("201801", 31);
+            _fakeBudgetRepo.AddBudget("201802", 280);
+            var totalAmount = _budgetService.TotalAmount(new DateTime(2017, 12, 31), new DateTime(2018, 2, 1));
+            AmountShouldBe(1041, totalAmount);
+        }
+    }
 
-		[TestMethod()]
-		public void DiffYear_With_Cross_MultiMonthBudgets()
-		{
-			_fakeBudgetRepo.AddBudget("201712", 31000);
-			_fakeBudgetRepo.AddBudget("201801", 31);
-			_fakeBudgetRepo.AddBudget("201802", 280);
-			var totalAmount = _budgetService.TotalAmount(new DateTime(2017, 12, 31), new DateTime(2018, 2, 1));
-			AmountShouldBe(1041, totalAmount);
-		}
-	}
+    public class FakeBudgetRepo : IBudgetRepo
+    {
+        private List<Budget> _budgets = new List<Budget>();
 
-	public class FakeBudgetRepo : IBudgetRepo
-	{
-		private List<Budget> _budgets = new List<Budget>();
+        public virtual List<Budget> GetAll()
+        {
+            return _budgets;
+        }
 
-		public virtual List<Budget> GetAll()
-		{
-			return _budgets;
-		}
-
-		public void AddBudget(string yearMonth, int amount)
-		{
-			_budgets.Add(new Budget() { YearMonth = yearMonth, Amount = amount });
-		}
-	}
+        public void AddBudget(string yearMonth, int amount)
+        {
+            _budgets.Add(new Budget() { YearMonth = yearMonth, Amount = amount });
+        }
+    }
 }
