dc1b01c40e40180ef5fe1bcadffff154cec48e60
Joey Chen <jay.hatelove@gmail.com>
2018-11-10 21:25:28 +0800

    extract method: IntervalAmount()

--- RefactorTdd/BudgetService.cs
+++ RefactorTdd/BudgetService.cs
@@ -34,7 +34,7 @@ namespace RefactorTdd
                     return 0;
                 }
 
-                return budget.DailyAmount() * period.IntervalDays(budget);
+                return IntervalAmount(budget, period);
             }
             else
             {
@@ -53,6 +53,11 @@ namespace RefactorTdd
 
                 return totalAmount;
             }
+        }
+
+        private static double IntervalAmount(Budget budget, Period period)
+        {
+            return budget.DailyAmount() * period.IntervalDays(budget);
         }
 
         private static bool IsSameMonth(DateTime start, DateTime end)
